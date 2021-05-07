9ee2da338ec8071c0c4498625074ebf4a55ab79a
Barbara Ferraz <barbaraferrazti@gmail.com>
2021-05-07 16:54:18 -0300

    BarChart integration

---
+++ frontend/src/utils/format.ts
@@ -0,0 +1,6 @@
+/// Código usado para arredondar o número
+
+export const round = (value: number, precision: number) => {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
